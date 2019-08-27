1b55bc8f84d8fcd8116591c4baa4ff9e2bb6e28a
frank paulsen <frobnic+ita@gmail.com>
2019-08-27 15:04:38 +0200

    inserted code

--- 0606A1Sortieren/Program.cs
+++ 0606A1Sortieren/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _0606A1Sortieren
 {
@@ -10,6 +6,25 @@ namespace _0606A1Sortieren
     {
         static void Main(string[] args)
         {
+            Boolean swap = false;
+            int[] unsorted = new int[] { 5, 2, 6, 4, 8, 3, 8, 7, 2, 4, 5 };
+            int t;
+
+            do
+            {
+                swap = false;
+                for (int i = 1; i < unsorted.Length; i++)
+                    if (unsorted[i - 1] > unsorted[i])
+                    {
+                        t = unsorted[i - 1];
+                        unsorted[i - 1] = unsorted[i];
+                        unsorted[i] = t;
+                        swap = true;
+                    }
+                foreach (int a in unsorted)
+                    Console.Write("{0} ", a);
+                Console.WriteLine();
+            } while (swap);
         }
     }
 }
